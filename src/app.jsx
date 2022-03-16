32258e565d76f88c115d822642cc9e70b92b9b4b
WasabiAddict <davidtltran@gmail.com>
2022-03-16 10:54:05 -0500

    sloppy changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import Greeter from "./components/Greeter";
 
 const App = () => {
-    return (
-        <div>
-            <h1>This is the way.</h1>
-        </div>
-    );
+    const [username, setUsername] = useState("")
+    comst [loaded, setLoaded] = useState(false);
+    console.log(username) //""
+
+    if(loaded) {
+        return (
+            <div>
+                <h1>This is the way.</h1>
+                <Greeter name="Josh" phrase="hello, there"/>
+                <Greeter name="Turner" phrase="you're lagging, "/>
+    
+                
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+    
+                <p>you're lagging lagging as {username} </p>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>website loading...</h1>
+                <button onCLick={() => setLoaded(true)}>load website!></button>
+            </div>
+        )
+    }
 }
 
 export default App;
